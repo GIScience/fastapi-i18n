81f41ec0eca092f1e9d8b235336f1d7a96f21ede
Levi Szamek <levi.szamek@heigit.org>
2025-10-29 15:59:23 +0100

    feat: add function to get locale setting

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+# CHANGELOG
+
+## Current Main
+
+- feat: add function to get current locale setting ([#2](https://github.com/GIScience/fastapi-i18n/pull/2))
--- tests/test_i18n.py
+++ tests/test_i18n.py
@@ -1,8 +1,10 @@
 from pathlib import Path
+from unittest.mock import Mock
 
 import pytest
 
-from fastapi_i18n import _
+from fastapi_i18n import _, get_locale
+from fastapi_i18n.main import i18n
 
 
 @pytest.fixture(autouse=True)
@@ -21,3 +23,25 @@ def test_translate(caplog):
     """Call translate without usage of the FastAPI dependency."""
     assert _("Hello from fastapi-i18n!") == "Hello from fastapi-i18n!"
     assert "FastAPI I18N translator is not set." in caplog.text
+
+
+def test_get_locale_default():
+    assert get_locale() == "en"
+
+
+def test_get_locale_custom(monkeypatch):
+    monkeypatch.setenv(
+        "FASTAPI_I18N_LOCALE_DEFAULT",
+        "de",
+    )
+    assert get_locale() == "de"
+
+
+@pytest.mark.asyncio
+async def test_get_locale_context():
+    mock = Mock()
+    mock.headers.get.return_value = "es"
+    gen = i18n(mock)
+    await anext(gen)
+
+    assert get_locale() == "es"
